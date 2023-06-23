73035d7178ac0aa2e2ff32f1f35bee598390b6f7
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-06-23 06:23:21 +0100

    Set up HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'production',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 };
